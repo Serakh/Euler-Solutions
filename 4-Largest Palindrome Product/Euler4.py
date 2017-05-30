ab111685c5846e233036d3f7e30fe7f51b34c1d7
Ross Volenec <Serakh@users.noreply.github.com>
2017-05-30 15:00:39 -0400

    Update Euler4.py

--- 4-Largest Palindrome Product/Euler4.py
+++ 4-Largest Palindrome Product/Euler4.py
@@ -3,7 +3,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.'''
 
 def palindrome(n=3):
     limit = int('9' * n) ** 2 #set our limit to the largest square of numbers with n digits
-    for i in range(11, limit, -1): # iterate from limit to 11 - the smallest possible palindrome
+    for i in range(limit, 11, -1): # iterate from limit to 11 - the smallest possible palindrome
         if str(i) == str(i)[::-1]:
             return 'The largest palindrome made from the product of two {}-digit numbers is: {}'.format(n, i)
         
