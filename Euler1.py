2e424bfc74bd54038e964a93716caf3d3f5ead48
Ross Volenec <Serakh@users.noreply.github.com>
2017-05-31 10:41:39 -0400

    Standardizing and relocating Euler1.py

--- Euler1.py
+++ Euler1.py
@@ -1,16 +1,9 @@
 '''If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
-
 Find the sum of all the multiples of 3 or 5 below 1000.'''
 
-def multiples(limit):
+def multiples(n=1000):
     mults = []
-    for i in range(1, limit):
+    for i in range(1, n):
         if i % 3 ==  0 or i % 5 == 0:
             mults.append(i)
-    return sum(mults)
-    
-limit = 1000
-print("The sum of multiples of 3 or 5 under " + str(limit) + " is: " + str(multiples(limit)))
-
-while True:
-    input("")
+    print("The sum of multiples of 3 or 5 under {} is {}".format(n, sum(mults)))
