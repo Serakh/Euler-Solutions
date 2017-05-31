46c0574af96dd9ac7eafb5b62d6f53cd8d92118a
Ross Volenec <Serakh@users.noreply.github.com>
2017-05-31 10:33:24 -0400

    Default n=20 as per problem.

--- 5-Smallest Multiple/Euler5.py
+++ 5-Smallest Multiple/Euler5.py
@@ -3,8 +3,10 @@ What is the smallest positive number that is evenly divisible by all of the numb
 
 from math import factorial
 
-def mult(n=10):
-  multiples = list(range(2, n+1))
-  for i in range(2*n, factorial(n),  n):
-    if all(i % j == 0 for j in multiples):
-      return i
+def mult(n=20):
+      multiples = list(range(2, n+1))
+          for i in range(2*n, factorial(n),  n):
+                if all(i % j == 0 for j in multiples):
+                    return i
+                
+print(mult())
