48152fe189b3470eb2471b492cca4b408516a69b
Ross Volenec <Serakh@users.noreply.github.com>
2017-05-31 11:01:32 -0400

    Standardize and relocate Euler5.py

---
+++ Euler5.py
@@ -0,0 +1,11 @@
+'''2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?'''
+
+from math import factorial
+
+def mult(n=20):
+    multiples = list(range(2, n+1))
+    for i in range(2*n, factorial(n),  n):
+        if all(i % j == 0 for j in multiples):
+            print("{} is the smallest number that can be divided by all numbers from 1 to {}.".format(i, n))
+            return None
